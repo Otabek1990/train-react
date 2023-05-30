f0a438416dabb177e78df78a5f59328831b34233
Fozilov Otabek <fozilovotabek424@gmail.com>
2023-05-30 20:02:19 +0500

    register-page-created

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div className='flex flex-col items-center p-10'>
+    <h1>register sahifa</h1>
+    <form action="" className='flex flex-col gap-4 items-center'>
+      <input type="text" className='border p-3 border-black' />
+      <input type="text" className='border p-3 border-black' />
+      <input type="text" className='border p-3 border-black' />
+    </form>
+  </div>
+  )
+}
+
+export default Register
