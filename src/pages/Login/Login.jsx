6900d63efef722002bcf532c3e7359209d87a777
Fozilov Otabek <fozilovotabek424@gmail.com>
2023-05-30 19:46:34 +0500

    login-page-modified

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
+    <div className='flex flex-col items-center p-10'>
       <h1>Login sahifa</h1>
+      <form action="">
+        <input type="text" className='border p-3 border-black' />
+      </form>
     </div>
   )
 }
